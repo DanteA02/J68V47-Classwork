097486ab2812f08b0ee055aeb22f62ee438927b0
30150478 <30150478@nescol.ac.uk>
2023-09-06 10:35:30 +0100

    lesson2

---
+++ src/lesson2input.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class lesson2input {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("What is your name? ");
+        String name = input.nextLine();
+        String firstName = "Bob";
+        System.out.println(firstName);
+        firstName = "Mary";
+        System.out.println(firstName);
+        System.out.print("Dante");
+    }
+}
